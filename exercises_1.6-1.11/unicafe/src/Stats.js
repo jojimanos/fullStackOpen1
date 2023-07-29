9813d66ce7c1b3330ba33ac14d13b2f4a464c0ec
jojimanos <manosgeorge7@gmail.com>
2023-07-29 11:31:22 +0300

    exercise 1.7

--- exercises_1.6-1.11/unicafe/src/Stats.js
+++ exercises_1.6-1.11/unicafe/src/Stats.js
@@ -1,10 +1,18 @@
 const Stats = ({good, neutral, bad}) => {
+    
+    const total = good + neutral + bad;
+    const average = good*1 + bad*(-1)
+    const goodPercentage = (good/total)*100
+
     return (
         <>
             <h1>Statistics</h1>
             <p>Godd {good}</p>
             <p>Neutral {neutral}</p>
             <p>Bad {bad}</p>
+            <p>Total {total}</p>
+            <p>Average {average}</p>
+            <p>Good percentage {goodPercentage} %</p>
         </>
     )
 }
